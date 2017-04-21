49f6dc70056c86ddc639c09b6dd06bb90c6db819
njunghans <junghans.nino@googlemail.com>
2017-04-21 17:43:41 +0200

    change menu

--- app/src/main/java/eu/junghans/learneasy/MainActivity.java
+++ app/src/main/java/eu/junghans/learneasy/MainActivity.java
@@ -80,17 +80,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_library) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_archive) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_marketplace) {
 
         }
 
